b5a80bc07a68e5b812a81b1ac0544bdca6597aaa
Mysticial <a-yee@u.northwestern.edu>
2015-04-17 23:02:26 -0700

    Upload source.

---
+++ src/Main.cpp
@@ -0,0 +1,26 @@
+/* Main.cpp
+ * 
+ * Author           : Alexander J. Yee
+ * Date Created     : 04/17/2015
+ * Last Modified    : 04/17/2015
+ * 
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "x86/cpu_x86.h"
+using namespace FeatureDetector;
+
+int main(){
+
+    cout << "CPU Vendor String: " << cpu_x86::get_vendor_string() << endl;
+    cout << endl;
+
+    cpu_x86::print_host();
+
+#if _WIN32
+    system("pause");
+#endif
+}
